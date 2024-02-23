ec5655e9e440e50b7bb8d5c481ee8f2d4cf2676a
leerlandais <leerlandais@gmail.com>
2024-02-23 19:22:07 +0100

    ça commence

---
+++ view/poetiniView.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/styles/style.css">
+    <title>Librarie Poetini</title>
+</head>
+    <body>
+        <div class="poetDiv">
+            <img src="/public/images/poetini_logo.png" alt="poetini_logo">
+        </div>
+    <script src="../public/scripts/script.js"></script>
+    </body>
+</html>
