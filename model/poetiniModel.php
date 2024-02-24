503e86e52d64201fc7002b0cb3fea951f04a49c1
leerlandais <leerlandais@gmail.com>
2024-02-24 11:17:37 +0100

    insértion message OK

---
+++ model/poetiniModel.php
@@ -0,0 +1,40 @@
+<?php
+
+function getMessages(PDO $db): array
+{
+{
+  
+    $sql = "SELECT * FROM poetini ORDER BY id ASC";
+    $query = $db->query($sql);
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    $query->closeCursor();
+    return $result; 
+}
+}
+
+
+function addMessages(PDO $db,
+                    string $firstname,
+                    string $lastname,
+                    string $usermail,
+                    string $userphone,
+                    string $message
+                    )
+{
+    $firstN = htmlspecialchars(strip_tags(trim($firstname)), ENT_QUOTES);
+    $lastN = htmlspecialchars(strip_tags(trim($lastname)), ENT_QUOTES);
+    $email = filter_var($usermail, FILTER_VALIDATE_EMAIL);
+    $phone = htmlspecialchars(strip_tags(trim($userphone)), ENT_QUOTES);
+    $texte = htmlspecialchars(strip_tags(trim($message)),ENT_QUOTES);
+    if (empty($firstN) || empty($lastN) || $email === false || empty($texte)) {
+        return false;
+    }
+
+    $sql = "INSERT INTO `poetini` (`user_nom`, `user_prenom`, `user_email`, `user_phone`, `user_message`) VALUES ('$firstN', '$lastN', '$email', '$phone', '$texte')";
+    try {
+    $db->exec($sql);
+    return true;
+} catch (Exception $e) {
+    return $e->getMessage();
+}
+}
--- public/index.php
+++ public/index.php
@@ -1,5 +1,34 @@
 <?php
 
+require_once "../config.php";
+require_once "../model/poetiniModel.php";
+
+try {
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT, DB_LOGIN, DB_PWD);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+$showMessages = getMessages($db);
+
+if (isset($_POST['user_nom'], $_POST['user_prenom'], $_POST['user_email'], $_POST['user_phone'], $_POST['user_message'])) {
+
+    
+    // on appelle la fonction d'insertion dans la DB (addLivreOr())
+    $insert = addMessages($db,$_POST['user_nom'], $_POST['user_prenom'], $_POST['user_email'], $_POST['user_phone'], $_POST['user_message']);
+    
+    // si l'insertion a réussi
+    if ($insert) {
+        // on redirige vers la page actuelle
+        header("Location: ./"); 
+        exit();
+    } else {
+        // sinon, on affiche un message d'erreur
+        $messageError = "Erreur avec l'insertion";
+    }
+
+}
+
+
 if(isset($_GET["p"])){
     switch($_GET["p"]){
         case 'home':
@@ -17,6 +46,10 @@ if(isset($_GET["p"])){
             case 'commander':
                 $title = "Commander un livre";
             include("../view/poetiniCommander.php");
+            break;
+            case 'private' :
+                $title = "page pour Chris";
+            include("../view/poetiniPrivate.php");
             break;
             default:
             $title = "Page 404";
