000ce6bbb98eba1d7c1bc323ecac7be1ea035d7c
leerlandais <leerlandais@gmail.com>
2024-02-25 09:53:20 +0100

    spinning images

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,10 +2,11 @@ const articleWindow = document.querySelectorAll(".articleWindow");
 
 
 for (i=0; i<articleWindow.length; i++) {
-    let articleSpin = articleWindow[i].addEventListener('click', spinArticle);
+    articleWindow[i].addEventListener('click', spinArticle);
 }
 
 function spinArticle () {
     this.classList.toggle("spinIt");
+
 }
 
